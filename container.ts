d779efbdf78396b0fe89c67729dd677c871e8ff5
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2023-06-28 06:40:34 +0000

    fix: make log options optional

--- container.ts
+++ container.ts
@@ -130,7 +130,7 @@ export class Container {
     return JSON.parse(res.body)
   }
 
-  async logs(id: string, options: {stdout?: true, stderr?: true}): Promise<InspectResponse> {
+  async logs(id: string, options?: {stdout?: true, stderr?: true}): Promise<InspectResponse> {
     const stdout = options ? Boolean(options.stdout).toString() : "true"
     const stderr = options ? Boolean(options.stderr).toString() : "true"
     const res = await this.client.get(
